1f0de2baaafee78568496adcc0251849d3f30cd5
Rafael Goncalves Dias <rafael.dias@teknisa.com>
2025-03-22 00:14:14 -0300

    project start

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { HelloModule } from './modules/hello/hello.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'), // Changed to code first aproach
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    HelloModule,
+    PrismaModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: './database/database_orm.sqlite',
+      autoLoadEntities: true,
+      synchronize: true,
+      migrations: ['../typeorm/migrations/*.ts'],
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
